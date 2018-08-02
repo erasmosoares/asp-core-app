244a8478e1c023fd07dc6858546bab943dab4b1d
Erasmo Soares <esaj@cesar.org.br>
2018-08-02 17:12:57 -0300

    Partial commit for vehicle repository

---
+++ vega/Persistence/VehicleRepository.cs
@@ -0,0 +1,12 @@
+using vega.Models;
+
+namespace vega.Persistence
+{
+    public class VehicleRepository
+    {
+        public Vehicle GetVehicle(int id)
+        {
+            return null;
+        }
+    }
+}
