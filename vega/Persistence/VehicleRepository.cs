03f82fea2835ae0af27033a0f6c239ca5ccbd2bb
Erasmo Soares de Araújo Júnior <erasmosaraujo@gmail.com>
2018-08-02 22:33:46 -0300

    Adding unit of work

--- vega/Persistence/VehicleRepository.cs
+++ vega/Persistence/VehicleRepository.cs
@@ -1,12 +1,38 @@
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 using vega.Models;
 
 namespace vega.Persistence
 {
-    public class VehicleRepository
+    public class VehicleRepository : IVehicleRepository
     {
-        public Vehicle GetVehicle(int id)
+        private readonly VegaDbContext context;
+        public VehicleRepository(VegaDbContext context)
         {
-            return null;
+            this.context = context;
+        }
+        public async Task<Vehicle> GetVehicle(int id, bool includeRelated = true)
+        {
+            if(!includeRelated)
+              return await context.Vehicles.FindAsync(id);
+
+            return await context.Vehicles
+                        .Include(v => v.Features)
+                        .ThenInclude(vf => vf.Feature)
+                        .Include(v => v.Model)
+                        .ThenInclude(m => m.Make)
+                        .SingleOrDefaultAsync(v => v.Id == id);
+
+        }
+
+        public void Add(Vehicle vehicle)
+        {
+            context.Vehicles.Add(vehicle);
+        }
+
+        public void Remove(Vehicle vehicle)
+        {
+            context.Remove(vehicle);
         }
     }
 }
--- vega/Startup.cs
+++ vega/Startup.cs
@@ -25,6 +25,13 @@ namespace vega
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            // transient - a separate instance for every user
+            // singleton - a single instance during appliction lifecycle
+            // scoped - a single instance for each request
+            services.AddScoped<IVehicleRepository, VehicleRepository>();
+
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+
             services.AddAutoMapper();
 
             services.AddDbContext<VegaDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("Default")));
